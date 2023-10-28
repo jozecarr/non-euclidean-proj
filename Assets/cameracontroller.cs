69cada4c4b52dadf4e89989207d18ed6d85c97e0
jozecarr <jxc1389@student.bham.ac.uk>
2023-10-28 04:30:31 +0100

    update camera script

--- Assets/cameracontroller.cs
+++ Assets/cameracontroller.cs
@@ -15,18 +15,7 @@ public class cameracontroller : MonoBehaviour
         transform.position = playerTrans.position;
 
         deltaMouse = Input.mousePosition - deltaMouse;
-        
-        // Modify the camera's rotation based on deltaMouse
-        float rotateX = -deltaMouse.y * sens;
-        float rotateY = deltaMouse.x * sens;
-
-        // Limit the rotation around the X axis to prevent Z rotation changes
-        float currentXRotation = transform.localEulerAngles.x;
-        float newRotationX = currentXRotation + rotateX;
-        newRotationX = Mathf.Clamp(newRotationX, -90f, 90f); // Limit the X rotation to -90 to 90 degrees
-
-        transform.localEulerAngles = new Vector3(newRotationX, transform.localEulerAngles.y + rotateY, 0);
-
+        transform.Rotate(0, deltaMouse.x, 0);
         deltaMouse = Input.mousePosition;
     }
 }
